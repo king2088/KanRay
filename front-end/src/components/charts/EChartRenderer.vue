a7a10a404ebaf2892382fb12de86d885c9f40963
king2088 <king6180@gmail.com>
2026-09-08 18:06:48 +0800

    feat: update EChartRenderer to support all new chart types

--- front-end/src/components/charts/EChartRenderer.vue
+++ front-end/src/components/charts/EChartRenderer.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="ec-chart" ref="el"></div>
+  <div class="ec-chart" ref="el" v-if="isEChartsType"></div>
+  <div v-else class="ec-chart-native">
+    <slot name="fallback"></slot>
+  </div>
 </template>
 
 <script setup>
-import { onBeforeUnmount, onMounted, ref, watch } from 'vue'
+import { onBeforeUnmount, onMounted, ref, watch, computed } from 'vue'
 import echarts from '@/utils/echarts'
-import { toEChartsOption } from '@/utils/chart-utils'
+import { OPTION_BUILDERS } from '@/config/chart-configs'
+import { getPalette } from '@/config/color-palettes'
 
 const props = defineProps({
   chartType: { type: String, required: true },
@@ -17,14 +21,30 @@ const el = ref(null)
 let chart = null
 let resizeObserver = null
 
+const isEChartsType = computed(() => {
+  if (!props.data) return true
+  const builder = OPTION_BUILDERS[props.chartType]
+  if (!builder) return false
+  const palette = props.options._palette || getPalette(props.options.colorPalette)
+  const opt = builder(props.data, props.options, palette)
+  return !opt._table && !opt._stat && !opt._progress && !opt._statTrend && !opt._map
+})
+
 function render() {
   if (!chart || !props.data) return
-  const opt = toEChartsOption(props.chartType, props.data, props.options)
-  if (!opt.backgroundColor) opt.backgroundColor = 'transparent'
-  if (opt._table || opt._stat) {
+  const builder = OPTION_BUILDERS[props.chartType]
+  if (!builder) return
+
+  const palette = props.options._palette || getPalette(props.options.colorPalette)
+  const opt = builder(props.data, props.options, palette)
+
+  // Handle non-ECharts returns - these are rendered natively by parent component
+  if (opt._table || opt._stat || opt._progress || opt._statTrend || opt._map) {
     chart.clear()
     return
   }
+
+  if (!opt.backgroundColor) opt.backgroundColor = 'transparent'
   chart.setOption(opt, true)
 }
 
@@ -35,7 +55,7 @@ onMounted(() => {
   render()
 })
 
-watch(() => [props.chartType, props.data, props.options?.title], render, { deep: false })
+watch(() => [props.chartType, props.data, props.options?.title, props.options?.colorPalette], render, { deep: false })
 
 onBeforeUnmount(() => {
   resizeObserver && resizeObserver.disconnect()
@@ -49,5 +69,13 @@ onBeforeUnmount(() => {
   width: 100%;
   height: 100%;
   min-height: 160px;
+}
+.ec-chart-native {
+  width: 100%;
+  height: 100%;
+  min-height: 160px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
