c737c110db16f5d2faeeff1a8e6691b7f69e3c8d
king2088 <king6180@gmail.com>
2026-09-10 09:20:31 +0800

    feat: ChartTypePanel default to bar category instead of all chart types

--- front-end/src/components/charts/ChartTypePanel.vue
+++ front-end/src/components/charts/ChartTypePanel.vue
@@ -50,7 +50,7 @@ defineProps({
 
 defineEmits(['update:chartType'])
 
-const activeCategory = ref('')
+const activeCategory = ref('bar')
 
 const filteredTypes = computed(() => {
   if (activeCategory.value === '') return CHART_TYPES
